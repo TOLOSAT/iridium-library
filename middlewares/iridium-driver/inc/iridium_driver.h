5bf92733e7ffec60652c67ae7d3ecae73071839b
Merlin Kooshmanian <mkooshmanian@gmail.com>
2023-10-22 16:06:25 +0000

    Prepare Iridium Driver for futur developpments

---
+++ middlewares/iridium-driver/inc/iridium_driver.h
@@ -0,0 +1,34 @@
+/**
+ * @file    iridium_driver.h
+ * @author  TO BE FILLED
+ * @brief   Header file for Iridium Driver
+ * @date    22/10/2023
+ *
+ * @copyright Copyright (c) TOLOSAT 2023
+ */
+
+/**
+ * @defgroup iridium-drv Iridium Driver
+ * @{
+ */
+
+#ifndef IRIDIUM_DRIVER_H
+#define IRIDIUM_DRIVER_H
+
+/******************************* Include Files *******************************/
+
+#include <stdint.h>
+
+/***************************** Macros Definitions ****************************/
+
+/***************************** Types Definitions *****************************/
+
+/*************************** Variables Declarations **************************/
+
+/*************************** Functions Declarations **************************/
+
+#endif /* IRIDIUM_DRIVER_H */
+
+/** 
+ * @} 
+ */
---
+++ middlewares/iridium-driver/src/iridium_driver.c
@@ -0,0 +1,20 @@
+/**
+ * @file    iridium_driver.c
+ * @author  TO BE FILLED
+ * @brief   Source file for Iridium Driver
+ * @date    22/10/2023
+ *
+ * @copyright Copyright (c) TOLOSAT 2023
+ */
+
+/******************************* Include Files *******************************/
+
+#include "iridium_driver.h"
+
+/***************************** Macros Definitions ****************************/
+
+/*************************** Functions Declarations **************************/
+
+/*************************** Variables Definitions ***************************/
+
+/*************************** Functions Definitions ***************************/
