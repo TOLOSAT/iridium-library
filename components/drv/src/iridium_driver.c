176c041705639367d176ae23f216f24746be1f6c
Merlin Kooshmanian <mkooshmanian@gmail.com>
2026-09-19 20:00:57 +0200

    ref: separate public includes and organize sources into components

--- components/drv/src/iridium_driver.c
+++ components/drv/src/iridium_driver.c
@@ -12,7 +12,7 @@
 
 #include "kernel.h"
 #include "drv/iridium_driver.h"
-#include "drv/at_commands.h"
+#include "at_commands.h"
 
 /***************************** Macros Definitions ****************************/
 
